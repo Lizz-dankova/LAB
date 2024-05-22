49aa689a194f89324d84d06aabc95d7db24b9c5e
Yelizaveta Dankova (Данькова Єлізавета) <144330950+Lizz-dankova@users.noreply.github.com>
2024-05-23 00:00:40 +0200

    Update lab1.py
    А ні, сподіваюсь тепер :(

--- lab1.py
+++ lab1.py
@@ -2,17 +2,7 @@ import math
 import time
 
 
-def measure_time(func, *args, index=None):
-    start_time = time.time()
-    result = func(*args)
-    end_time = time.time()
-    execution_time = end_time - start_time
-    if index is not None:
-        result = result[index]
-    return result, execution_time
-
-
-def parser(obj, n):
+def parcer(obj, n):
     args = [iter(obj)] * n
     return zip(*args)
 
@@ -45,35 +35,32 @@ def convert_from_hex(hex_num):
     hexs = []
     decs = []
     if len(hex_num) % 8 == 0:
-        hexs = [''.join(i) for i in parser(hex_num, 8)]
+        hexs = [''.join(i) for i in parcer(hex_num, 8)]
     else:
         zeros = 8 * n - len(hex_num)
         str_zeros = zeros * '0'
         final_hex = str_zeros + hex_num
-        hexs = [''.join(i) for i in parser(final_hex, 8)]
+        hexs = [''.join(i) for i in parcer(final_hex, 8)]
     for i in range(len(hexs)):
         decs.append(int(hexs[i], 16))
     decs.reverse()
     return decs
 
 
-def convert_result_to_hex(result):
-    return convert_to_hex(result)
-
-num_1_A = input('Перше число: ')
-num_2_B = input('Друге число: ')
-
+num_1_A = str(input('Enter the first number: '))
+num_2_B = str(input('Enter the second number: '))
 
 A = convert_from_hex(num_1_A)
 B = convert_from_hex(num_2_B)
+
 
 def LongAddition(a, b):
     max_len = max(len(a), len(b))
     carry = 0
     sum = []
     for i in range(max_len):
-        temp_A = a[i] if i < len(a) else 0
-        temp_B = b[i] if i < len(b) else 0
+        temp_A = int(a[i]) if i < len(a) else 0
+        temp_B = int(b[i]) if i < len(b) else 0
         temp = temp_A + temp_B + carry
         sum.append(temp & (2 ** 32 - 1))
         carry = temp >> 32
@@ -82,31 +69,31 @@ def LongAddition(a, b):
     return sum
 
 
-def LongSubtraction(a, b):
+def LongSubstraction(a, b):
     borrow = 0
     sub = []
     max_len = max(len(a), len(b))
     for i in range(max_len):
-        temp_A = a[i] if i < len(a) else 0
-        temp_B = b[i] if i < len(b) else 0
+        temp_A = int(a[i]) if i < len(a) else 0
+        temp_B = int(b[i]) if i < len(b) else 0
         temp = temp_A - temp_B - borrow
         if temp >= 0:
             sub.append(temp)
             borrow = 0
         else:
-            sub.append(temp + (2 ** 32))
+            sub.append((2 ** 32 + temp))
             borrow = 1
-    if borrow:
+    if borrow != 0:
         return None
-    while len(sub) > 1 and sub[-1] == 0:
-        sub.pop()
-    return sub
+    else:
+        return sub
 
 
 def LongMultiply(a, b):
     if LongCompare(a, b) == -1:
-        a, b = b, a
+        return LongMultiply(b, a)
 
+    max_len = max(len(a), len(b))
     mul = [0] * (len(a) + len(b))
 
     for i in range(len(b)):
@@ -115,6 +102,7 @@ def LongMultiply(a, b):
             temp = mul[i + j] + a[j] * b[i] + carry
             mul[i + j] = temp & (2 ** 32 - 1)
             carry = temp >> 32
+
         if carry > 0:
             mul[i + len(a)] += carry
 
@@ -125,21 +113,22 @@ def LongMultiply(a, b):
 
 
 def LongDivideModule(a, b):
-    if a == b:
-        return [1], [0]
     k = BitLength(b)
-    remainder = a.copy()
-    quotient = [0]
+    remainder = a
+    div = []
     while LongCompare(remainder, b) != -1:
         t = BitLength(remainder)
         c = LongShiftBitsToHigh(b, t - k)
-        if LongCompare(remainder, c) < 0:
+        if LongCompare(remainder, c) == -1:
             t -= 1
+            while b[0] == 0:
+                b = b[1:]
             c = LongShiftBitsToHigh(b, t - k)
-        remainder = LongSubtraction(remainder, c)
-        temp = LongShiftBitsToHigh([1], t - k)
-        quotient = LongAddition(quotient, temp)
-    return quotient, remainder
+        remainder = LongSubstraction(remainder, c)
+        div = LongAddition(div, LongShiftBitsToHigh([1], t - k))
+        while b[0] == 0:
+            b = b[1:]
+    return [div, remainder]
 
 
 def LongSquare(a):
@@ -152,103 +141,145 @@ def LongPower(a, b):
         if BitCheck(b, i) == 1:
             pow = LongMultiply(pow, a)
         a = LongMultiply(a, a)
+        while len(a) < len(pow):
+            a.append(0)
     return pow
 
 
 def LongCompare(a, b):
-    while len(a) > 1 and a[-1] == 0:
-        a.pop()
-    while len(b) > 1 and b[-1] == 0:
-        b.pop()
+    if a != [0]:
+        while a[len(a) - 1] == 0:
+            del a[len(a) - 1]
+    if b != [0]:
+        while b[len(b) - 1] == 0:
+            del b[len(b) - 1]
     if len(a) == len(b):
-        for i in range(len(a) - 1, -1, -1):
-            if a[i] != b[i]:
-                return 1 if a[i] > b[i] else -1
-        return 0
-    return 1 if len(a) > len(b) else -1
+        i = max(len(a), len(b)) - 1
+        while a[i] == b[i]:
+            i -= 1
+            if i == -1:
+                return 0
+        else:
+            if a[i] > b[i]:
+                return 1
+            else:
+                return -1
+    elif len(a) > len(b):
+        return 1
+    else:
+        return -1
 
 
 def LongShiftDigitsToHigh(n, l):
-    return [0] * l + n
+    for i in range(l):
+        n.insert(0, 0)
+    return n
 
 
-def LongShiftDigitsToLow(n, amount):
-    return n[amount:] if len(n) > amount else [0]
-
-
-def LongShiftBitsToHigh(number, width_shift):
-    if width_shift <= 0:
-        return number.copy()
-    remainder = width_shift % 32
-    width_shift //= 32
-    result = LongShiftDigitsToHigh(number.copy(), width_shift)
-    if remainder > 0:
-        for _ in range(remainder):
-            last_bit = (result[-1] >> 31) & 1
-            for j in range(len(result) - 1, 0, -1):
-                result[j] = ((result[j] << 1) | ((result[j - 1] >> 31) & 1)) & 0xFFFFFFFF
-            result[0] = (result[0] << 1) & 0xFFFFFFFF
-            if last_bit != 0:
-                result.append(last_bit)
-    while len(result) > 1 and result[-1] == 0:
-        result.pop()
+def LongShiftBitsToLow(n, amount):
+    if amount // 32 >= len(n):
+        return convert_from_hex('0')
+    if amount % 32 == 0:
+        return LongShiftDigitsToLow(n, amount // 32)
+    b = 32 - amount % 32
+    k = 0 if n[len(n) - 1] >> 32 - b != 0 else 1
+    result = [0] * (len(n) - k - amount // 32)
+    if k == 0:
+        result[len(n) - 1] = n[len(n) - 1] >> 32 - b
+        i = len(result) - 2
+    else:
+        i = len(result) - 1
+    for j in reversed(range(amount // 32 + 1, len(n))):
+        result[i] = (n[j] << b) & (2 ** 32 - 1) | n[j - 1] >> 32 - b
+        i -= 1
     return result
 
 
-def LongShiftBitsToLow(number, width_shift):
-    if width_shift <= 0:
-        return number
-    remainder = width_shift % 32
-    width_shift //= 32
-    result = number[width_shift:] if width_shift < len(number) else [0]
-    if remainder > 0:
-        for _ in range(remainder):
-            last_bit = (result[0] & 1) << 31
-            for j in range(len(result) - 1):
-                result[j] = ((result[j] >> 1) | ((result[j + 1] & 1) << 31)) & 0xFFFFFFFF
-            result[-1] = ((result[-1] >> 1) | last_bit) & 0xFFFFFFFF
-    while len(result) > 1 and result[-1] == 0:
-        result.pop()
+def LongShiftDigitsToLow(n, amount):
+    if len(n) - amount <= 0:
+        return [0]
+    i = amount - 1
+    while i > -1:
+        del n[i]
+        i -= 1
+    return n
+
+
+def LongShiftBitsToHigh(n, amount):
+    if amount % 32 == 0:
+        return LongShiftDigitsToHigh(n, amount // 32)
+    b = 32 - amount % 32
+    k = 1 if n[len(n) - 1] >> b != 0 else 0
+    result = [0] * (len(n) + k + amount // 32)
+    if k == 1:
+        result[len(n) - 1] = n[len(n) - 1] >> b
+        i = len(result) - 2
+    else:
+        i = len(result) - 1
+    for j in reversed(range(1, len(n))):
+        result[i] = (n[j] << 32 - b) & (2 ** 32 - 1) | n[j - 1] >> b
+        i -= 1
+    result[i] = (n[0] << 32 - b) & (2 ** 32 - 1)
     return result
 
 
 def BitCheck(a, i):
-    return (a[i // 32] >> (i % 32)) & 1
+    c = i % 32
+    j = i // 32
+    return (a[j] >> c) & 1
 
 
 def BitLength(a):
-    return (len(a) - 1) * 32 + a[-1].bit_length()
+    return (len(a) - 1) * 32 + a[len(a) - 1].bit_length()
 
-sum_result = LongAddition(A, B)
-sum_hex = convert_result_to_hex(sum_result)
-sub_result = LongSubtraction(A, B)
-sub_hex = convert_result_to_hex(sub_result)
-mul_result = LongMultiply(A, B)
-mul_hex = convert_result_to_hex(mul_result)
-divide_result = LongDivideModule(A, B)
-div = convert_result_to_hex(divide_result[0]) if divide_result[0] != [] else '0'
-div_mod = convert_result_to_hex(divide_result[1])
-pow_result = LongPower(A, B)
-pow_hex = convert_result_to_hex(pow_result)
-square_result = LongSquare(A)
-square = convert_result_to_hex(square_result)
-#pow = convert_to_hex(LongPower(A, B))
 
-#Time
-pow_result, pow_time = measure_time(LongPower, A, B)
+def measure_time(func, *args, index=None):
+    start_time = time.time()
+    result = func(*args)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    if index is not None:
+        result = result[index]
+    return result, execution_time
+
+
+# Time measurement and results calculation
 sum_result, sum_time = measure_time(LongAddition, A, B)
-sub_result, sub_time = measure_time(LongSubtraction, A, B)
+sub_result, sub_time = measure_time(LongSubstraction, A, B)
 mul_result, mul_time = measure_time(LongMultiply, A, B)
 divide_result, div_time = measure_time(LongDivideModule, A, B)
 square_result, square_time = measure_time(LongSquare, A)
 pow_result, pow_time = measure_time(LongPower, A, B)
 
+# Convert results to hex
+sum_hex = convert_to_hex(sum_result) if sum_result is not None else "Error"
+sub_hex = convert_to_hex(sub_result) if sub_result is not None else "Error"
+mul_hex = convert_to_hex(mul_result) if mul_result is not None else "Error"
 
-print(f'Додавання: {sum_hex}, Час виконання: {sum_time} секунд')
-print(f'Віднімання: {sub_hex}, Час виконання: {sub_time} секунд')
-print(f'Множення: {mul_hex}, Час виконання: {mul_time} секунд')
-print(f'Ділення: {div}, Час виконання: {div_time} секунд' )
-print(f'Остача: {div_mod}')
-print(f'Піднесення до квадрату: {square}, Час виконання: {square_time} секунд')
-print(f'Піднесення у степінь: {pow_hex}, Час виконання: {pow_time} секунд')
+div_hex = convert_to_hex(divide_result[0]) if divide_result[0] != [] else '0'
+div_mod_hex = convert_to_hex(divide_result[1])
+
+square_hex = convert_to_hex(square_result) if square_result is not None else "Error"
+pow_hex = convert_to_hex(pow_result) if pow_result is not None else "Error"
+
+# Print results
+print(f'The result of addition: {sum_hex}')
+print(f'Time taken for addition: {sum_time} seconds')
+
+print(f'The result of substraction: {sub_hex}')
+print(f'Time taken for substraction: {sub_time} seconds')
+
+print(f'The result of multiplication: {mul_hex}')
+print(f'Time taken for multiplication: {mul_time} seconds')
+
+print(f'The result of dividing: {div_hex}')
+print(f'Time taken for dividing: {div_time} seconds')
+
+print(f'The result of remainder of dividing: {div_mod_hex}')
+
+print(f'The result of elevation to the square: {square_hex}')
+print(f'Time taken for square: {square_time} seconds')
+
+print(f'The result of elevation: {pow_hex}')
+print(f'Time taken for power: {pow_time} seconds')
 
